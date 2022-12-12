40bfdfb2a5f55185bdf829f4f2c671a004ac81fd
Zayon Nieves <zayon.nieves1@gmail.com>
2022-12-12 18:15:45 -0500

    added files for user input through console

---
+++ calculator.js
@@ -0,0 +1,17 @@
+
+// Step #1: explain to user how to use the program
+console.log(`
+Welcome to the calculator!
+
+This program will ask you for 3 seperate pieces of data;
+
+    1. A first number
+    2. A second number
+    3. A mathematical operator (+, -, X, /)
+
+The progam will then apply the operator to the 2 numbers and produce a result.
+-------------------------------------------------------------------------------
+\n`);
+
+
+// Step #2: get inputs from user
